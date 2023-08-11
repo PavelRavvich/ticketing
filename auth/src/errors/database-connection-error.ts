55bd5d87859c3905cbe371647d2d0654e52e2cde
pavel <triodjangopiter@yandex.ru>
2023-08-11 08:45:09 +0300

    types ref

--- auth/src/errors/database-connection-error.ts
+++ auth/src/errors/database-connection-error.ts
@@ -1,8 +1,8 @@
 import { CustomError } from "./custom-error";
 
 export class DatabaseConnectionError extends CustomError {
-  statusCode = 500;
-  reason = "Error connecting to database";
+  statusCode: number = 500;
+  reason: string = "Error connecting to database";
 
   constructor() {
     super("Error connecting to database");
--- auth/src/errors/not-found-error.ts
+++ auth/src/errors/not-found-error.ts
@@ -1,7 +1,7 @@
 import { CustomError } from "./custom-error";
 
 export class NotFoundError extends CustomError {
-  statusCode = 404;
+  statusCode: number = 404;
 
   constructor() {
     super("Route not found");
