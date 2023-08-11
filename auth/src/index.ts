c5e79a34521e4ea95c76f5a5f4930e57f4b53b3c
pavel <triodjangopiter@yandex.ru>
2023-08-11 15:21:26 +0300

    impl JWT_KEY and cookies generation

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,6 +2,7 @@ import express, { Express } from 'express';
 import 'express-async-errors';
 import { json } from 'body-parser';
 import mongoose from 'mongoose';
+import cookieSession from "cookie-session";
 
 import { sinnInRouter } from './routes/sign-in';
 import { sinnUpRouter } from './routes/sign-up';
@@ -11,7 +12,14 @@ import { errorHandler } from './middlewares/error-handler';
 import { NotFoundError } from "./errors/not-found-error";
 
 const app: Express = express();
+app.set('trust proxy', true);
 app.use(json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: true,
+  })
+);
 
 app.use(sinnInRouter);
 app.use(sinnUpRouter);
@@ -25,6 +33,10 @@ app.all('*', async (): Promise<void> => {
 app.use(errorHandler);
 
 const start = async (): Promise<void> => {
+  if (!process.env.JWT_KEY) {
+    throw new Error("JWT_KEY must be defined");
+  }
+
   try {
     await mongoose.connect("mongodb://auth-mongo-srv:27017/auth");
     console.log("Connected to MongoDB");
