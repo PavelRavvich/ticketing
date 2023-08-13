16efd5eb636218d2b5479b6e4fdba5584a4ce69c
pavel <triodjangopiter@yandex.ru>
2023-08-13 13:51:01 +0300

    common lib upd error-handler v1.0.5

--- common/src/middlewares/error-handler.ts
+++ common/src/middlewares/error-handler.ts
@@ -12,6 +12,7 @@ const errorHandler = (
     return res.status(err.statusCode).send({ errors: err.serializeErrors() });
   }
 
+  console.error(err);
   res.status(500).send({
     errors: [{ message: "Something went wrong" }]
   });
