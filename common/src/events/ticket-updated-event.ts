25c495f142b3332d71b366d4372e81973b0be175
pavel <triodjangopiter@yandex.ru>
2023-08-14 22:13:28 +0300

    impl common events logic

---
+++ common/src/events/ticket-updated-event.ts
@@ -0,0 +1,12 @@
+import { Subjects } from './subjects';
+import { Event } from "./base-listener";
+
+export interface TicketUpdatedEvent extends Event {
+  subject: Subjects.TicketUpdated;
+  data: {
+    id: string;
+    title: string;
+    price: number;
+    userId: string;
+  }
+}
