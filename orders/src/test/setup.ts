78541277462d54a1729de3622983c6435525137b
pavel <triodjangopiter@yandex.ru>
2023-08-15 16:36:47 +0300

    add k8s and skaffold cons and startup fs for Order service

---
+++ orders/src/test/setup.ts
@@ -0,0 +1,41 @@
+import { MongoMemoryServer } from 'mongodb-memory-server';
+import mongoose from 'mongoose';
+import jwt from "jsonwebtoken";
+
+let mongo: MongoMemoryServer;
+
+jest.mock("../nats-wrapper");
+
+beforeAll(async (): Promise<void> => {
+  process.env.JWT_KEY = "test-jwt-key";
+
+  mongo = await MongoMemoryServer.create();
+  const mongoUri = mongo.getUri();
+  await mongoose.connect(mongoUri, {});
+});
+
+beforeEach(async (): Promise<void> => {
+  jest.clearAllMocks();
+  const collections = await mongoose.connection.db.collections();
+
+  for (const collection of collections) {
+    await collection.deleteMany({});
+  }
+});
+
+afterAll(async () => {
+  if (mongo) {
+    await mongo.stop();
+  }
+  await mongoose.connection.close();
+});
+
+export const signIn = (): string[] => {
+  const token = jwt.sign({
+    id: new mongoose.Types.ObjectId().toHexString(),
+    email: "test@test.com"
+  }, process.env.JWT_KEY!);
+  const sessionJSON = JSON.stringify({ jwt: token });
+  const base64Session = Buffer.from(sessionJSON).toString("base64");
+  return [`session=${base64Session}`];
+};
