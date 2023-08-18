b3be848b770383e6ca746c125ddb966a8fd3ddb2
pavel <triodjangopiter@yandex.ru>
2023-08-18 16:59:01 +0300

    k8s and skaffold conf for payments service

---
+++ payments/src/app.ts
@@ -0,0 +1,26 @@
+import express, { Express } from 'express';
+import 'express-async-errors';
+import { json } from 'body-parser';
+import cookieSession from "cookie-session";
+import { errorHandler, NotFoundError, currentUser } from "@pravvich-tickets/common";
+
+
+const app: Express = express();
+app.set('trust proxy', true);
+app.use(json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: process.env.NODE_ENV !== 'test',
+  })
+);
+app.use(currentUser);
+
+
+app.all('*', async (): Promise<void> => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+export { app };
